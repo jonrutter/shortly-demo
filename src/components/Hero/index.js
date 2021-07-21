1d43c5a81d929282792551fd8d77dd39e99dc524
Jon Rutter <contact@jonrutter.com>
2021-07-21 17:22:45 -0400

    Drafted Hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+// styles
+import s from './Hero.module.css';
+
+// components
 import Section from '../Section';
+
+// images
+import HeroImg from '../../images/illustration-working.svg';
 
 const Hero = () => {
   return (
     <Section>
-      <h2>Hero Section</h2>
+      <div className={s.wrapper}>
+        <div className={s.wrapperRight}>
+          <img
+            className={s.img}
+            src={HeroImg}
+            alt="Graphical illustration of a person working on a computer"
+          />
+        </div>
+        <div className={s.wrapperLeft}>
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button className={s.btn}>Get Started</button>
+        </div>
+      </div>
     </Section>
   );
 };
